bb11e192516f9a1cce71c1e0b9eca88358feddcb
jonathanrobertp <jonathan.putera@bfi.co.id>
2022-08-22 16:07:40 +0700

    Commit New Test Case

--- Keywords/DBKeyword.groovy
+++ Keywords/DBKeyword.groovy
@@ -3,7 +3,7 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.katalon.plugin.keyword.connection.DBType as DBType
+//import com.katalon.plugin.keyword.connection.DBType as DBType
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
